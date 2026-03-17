32785263de4523a9dfe54992d9619041a0f94684
FLIPPED-RN <rnazarov142@gmail.com>
2026-03-17 17:35:22 +0200

    create project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import LoadingPage from "./pages/LoadingPage"
+
+function App() {
+  return (
+    <>
+      <div className="w-xl mx-auto border border-white h-screen">
+        <LoadingPage />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import RBMLogo from '../assets/rbm.png'
+
+export default function LoadingPage() {
+	return (
+		<div>
+			<div>
+				<img src={RBMLogo} alt="лалалал"/>
+			</div>
+		</div>
+	)
+}
