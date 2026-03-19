9fe2c8fbe6176c369a7b591bc63334894450cfe8
FLIPPED-RN <rnazarov142@gmail.com>
2026-03-19 20:07:29 +0200

    feat: initial commit

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,10 +1,12 @@
 import RBMLogo from '../assets/rbm.png'
+import { Button } from '../components/ui/button'
 
 export default function LoadingPage() {
 	return (
 		<div>
 			<div>
-				<img src={RBMLogo} alt="лалалал"/>
+				<img src={RBMLogo} alt="лалалал" className=''/>
+				<Button>Hello!</Button>
 			</div>
 		</div>
 	)
